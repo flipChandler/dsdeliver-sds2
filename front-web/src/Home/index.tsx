7569518efe215ca4cbadee33a0b1564a72787462
flipChandler <felipe.santos11@hotmail.com>
2021-01-28 22:35:13 -0300

    Add Routes and StepsHeader

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,6 +1,7 @@
 import { ReactComponent } from '*.svg';
 import './styles.css';
 import { ReactComponent as MainImage } from './main.svg';
+import { Link } from 'react-router-dom';
 import Footer from '../Footer';
 
 
@@ -16,9 +17,9 @@ function Home() {
                         <h3 className="home-subtitle">
                             Escolha o seu pedido e em poucos minutos <br />levaremos na sua porta
                     </h3>
-                        <a href="orders" className="home-btn-order">
+                        <Link to="/orders" className="home-btn-order">
                             FAZER PEDIDO
-                    </a>
+                    </Link>
                     </div>
                     <div className="home-images">
                         <MainImage />
--- front-web/src/NavBar/index.tsx
+++ front-web/src/NavBar/index.tsx
@@ -1,12 +1,13 @@
 import { ReactComponent } from '*.svg';
 import './styles.css';
 import { ReactComponent as Logo} from './logo.svg'; // cria tag do React
+import { Link } from 'react-router-dom';
 
 function NavBar() {
     return (
         <nav className="main-navbar">
             <Logo />
-            <a href="home" className="logo-text">DS Delivery</a>
+            <Link to="/" className="logo-text">DS Delivery</Link>
         </nav>
     )
 }
