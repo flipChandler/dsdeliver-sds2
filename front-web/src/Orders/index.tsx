bba5a6c8d89dc99a3d587a34e78e8a7297d989ea
flipChandler <felipe.santos11@hotmail.com>
2021-02-01 18:34:04 -0300

    add ProductCard ProductList api.ts axios

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,27 @@
 import './styles.css';
 import StepsHeader from './StepsHeader';
+import ProductsList from './ProductsList';
+import { useEffect , useState } from 'react';
+import { Product } from './types';
+import { fetchProducts } from '../api';
 
+//responsável por trazer dinamicamicamente os dados via api
 function Orders() {
+
+    const [products, setProducts] = useState<Product[]>([]); {/* products vai inicializar com uma lista vazia */}
+
+
+    useEffect(() => {
+        fetchProducts()
+            .then(response => setProducts(response.data))
+            .catch(error => console.log(error))
+    }, []);
+
+
     return (
         <div className="orders-container">
             <StepsHeader />
+            <ProductsList products={products}/>
         </div>
     )
 }
